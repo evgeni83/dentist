f0ec4dd267a27a239a93a92d2fc21ebee22aca26
evgeni83 <evgeni.daniluk@yandex.ru>
2020-06-27 17:13:15 +0300

    Сверстана секция Услуги

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,28 @@
-$(document).ready(function () {
+jQuery(function ($) {
   $(".burger-btn").on("click", () => {
     $("html, body").toggleClass("no-scroll");
     $(".nav-menu--header, .burger-btn__item").toggleClass("active");
   })
+
+  $(".services-section__slider").slick({
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 });
